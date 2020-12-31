1f5ada42d2792b2b5939f3dd996f87363bec9f4a
Anas <ameziani@everis.com>
2020-12-31 22:58:22 +0100

    Bot returns lyrics to private chat but a bit slow

--- botTelegram.py
+++ botTelegram.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 import os, telegram, logging, lyricsgenius, re
-from telegram.ext import Updater, CommandHandler, InlineQueryHandler
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler, CallbackQueryHandler, CallbackContext, ChosenInlineResultHandler
 from pprint import pprint
-from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, Message
 
 load_dotenv()
 
@@ -14,48 +14,85 @@ genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
 
 print("Bot working!\n", bot.get_me())
 
-updater = Updater(token=TELEGRAM_BOT_TOKEN)
-dispatcher = updater.dispatcher
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+
+def search(song_name):
+    songs=genius.search_songs(song_name)
+    list_songs={}
+    for index, item in enumerate(songs['hits']):
+        full_title=item['result']['full_title']
+        lyrics_url=item['result']['url']
+        thumb_url=item['result']['header_image_thumbnail_url']
+        artist=item['result']['primary_artist']['name']      
+        
+        attributes={}
+        attributes['full_title']=full_title
+        attributes['lyrics_url']=lyrics_url
+        attributes['thumb_url']=thumb_url
+        attributes['artist']=artist
+        
+        list_songs[index]=attributes
+    return list_songs
+
+def search_lyrics(selectedIndex, songs):
+    full_title=songs[int(selectedIndex)]['full_title']
+    artist=songs[int(selectedIndex)]['artist']
+    lyrics=genius.search_song(full_title, artist).lyrics
+    return lyrics
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hola, don Pepito!")
 
 def song(update, context):
-    result=""
-    name_song=re.sub(r"\/song ", "", update.message.text)
+    song_name=re.sub(r"\/song ", "", update.message.text)
     print("----------------------------------------------------------")
-    print(name_song)
+    print(song_name)
     print("----------------------------------------------------------")
-    songs=genius.search_songs(name_song)
-    for index, item in enumerate(songs['hits']):
-        result+=""+str(index+1)+".  "+item['result']['full_title']+"\n"
+    result=search(song_name)
     context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
-def inline_song(update, context):
+
+globalUpdate={}
+
+def inline_song(update: Update, context: CallbackContext):
     query=update.inline_query.query
     if not query:
         return
     results=list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title="Results",
-            input_message_content=InputTextMessageContent(query.upper())
+    global globalUpdate
+    globalUpdate=update
+    last_searched_songs=search(query)
+    for index, song in last_searched_songs.items():
+        results.append(
+            InlineQueryResultArticle(
+                id=index,
+                thumb_url=song['thumb_url'],
+                title=song['full_title'],
+                url=song['lyrics_url'],
+                hide_url=True,
+                input_message_content=InputTextMessageContent(song['lyrics_url']+"\n"+"Check @GenyusBot for the lyrics!")
+            )
         )
-    )
     context.bot.answer_inline_query(update.inline_query.id, results)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
 
-song_handler = CommandHandler('song', song)
-dispatcher.add_handler(song_handler)
+def on_result_chosen(update: Update, context: CallbackContext):
+    songs=search(update.chosen_inline_result.query)
+    selected_index = update.chosen_inline_result.result_id
+    lyrics=search_lyrics(selected_index, songs)
+    bot.send_message(update.chosen_inline_result.from_user.id, text=lyrics)
 
-inline_song_handler=InlineQueryHandler(inline_song)
+
+updater = Updater(token=TELEGRAM_BOT_TOKEN)
+dispatcher = updater.dispatcher
+
+inline_song_handler = InlineQueryHandler(inline_song, run_async=True)
 dispatcher.add_handler(inline_song_handler)
 
+result_chosen_handler = ChosenInlineResultHandler(on_result_chosen, run_async=True)
+dispatcher.add_handler(result_chosen_handler)
+
 updater.start_polling()
+
 updater.idle()
