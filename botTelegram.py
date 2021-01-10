50d969e1672b1f2c711419a5dbe3c05db099f1d7
Anas <ameziani@everis.com>
2021-01-10 01:41:33 +0100

    Faster lyrics finder using multithreding and self made scrape function

--- botTelegram.py
+++ botTelegram.py
@@ -1,13 +1,15 @@
-from dotenv import load_dotenv
-import os, telegram, logging, lyricsgenius, re
-from telegram.ext import Updater, CommandHandler, InlineQueryHandler, CallbackQueryHandler, CallbackContext, ChosenInlineResultHandler
+import os, telegram, logging, lyricsgenius, re, scrape, tokens
+from telegram.ext import (Updater,
+                          CommandHandler,
+                          InlineQueryHandler,
+                          CallbackQueryHandler,
+                          CallbackContext,
+                          ChosenInlineResultHandler)
 from pprint import pprint
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, Message
 
-load_dotenv()
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-GENIUS_ACCESS_TOKEN = os.getenv("GENIUS_ACCESS_TOKEN")
+TELEGRAM_BOT_TOKEN = tokens.TELEGRAM_BOT_TOKEN
+GENIUS_ACCESS_TOKEN = tokens.GENIUS_ACCESS_TOKEN
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
@@ -16,16 +18,22 @@ print("Bot working!\n", bot.get_me())
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+global lyrics_searched
+lyrics_searched=list()
 
 def search(song_name):
     songs=genius.search_songs(song_name)
     list_songs={}
+    lyrics_to_be_searched=list()
     for index, item in enumerate(songs['hits']):
+        if index>=5:
+            break
         full_title=item['result']['full_title']
         lyrics_url=item['result']['url']
         thumb_url=item['result']['header_image_thumbnail_url']
         artist=item['result']['primary_artist']['name']      
-        
+        lyrics_to_be_searched.append(lyrics_url)
+
         attributes={}
         attributes['full_title']=full_title
         attributes['lyrics_url']=lyrics_url
@@ -33,37 +41,32 @@ def search(song_name):
         attributes['artist']=artist
         
         list_songs[index]=attributes
+    global lyrics_searched
+    scrape.get_lyrics(lyrics_to_be_searched)
+    lyrics_searched=dict()
+    lyrics_searched=scrape.lyrics_searched
+
     return list_songs
 
-def search_lyrics(selectedIndex, songs):
-    full_title=songs[int(selectedIndex)]['full_title']
-    artist=songs[int(selectedIndex)]['artist']
-    lyrics=genius.search_song(full_title, artist).lyrics
-    return lyrics
-
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hola, don Pepito!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, I'm a genius bot. You can use me inline just by typing my name an the name of the song or artist i.e @GenyusBot stromae")
 
-def song(update, context):
-    song_name=re.sub(r"\/song ", "", update.message.text)
-    print("----------------------------------------------------------")
-    print(song_name)
-    print("----------------------------------------------------------")
-    result=search(song_name)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
-
-
-globalUpdate={}
+# def song(update, context):
+#     song_name=re.sub(r"\/song ", "", update.message.text)
+#     result=search(song_name)
+#     context.bot.send_message(chat_id=update.effective_chat.id, text="[Google](www.google.es)", parse_mode='MarkdownV2')
 
 def inline_song(update: Update, context: CallbackContext):
     query=update.inline_query.query
     if not query:
         return
     results=list()
-    global globalUpdate
-    globalUpdate=update
-    last_searched_songs=search(query)
-    for index, song in last_searched_songs.items():
+    songs=search(query)
+    for index, song in songs.items():
+        global lyrics_searched
+        messageContent=song['full_title']+"\n\n"+lyrics_searched[song['lyrics_url']]+"\n\n"+song['lyrics_url']
+        if len(messageContent)>4096:
+            messageContent=song['full_title']+"\n\nLyrics too long to put them here. Please go to:\n\n"+song['lyrics_url']
         results.append(
             InlineQueryResultArticle(
                 id=index,
@@ -71,27 +74,41 @@ def inline_song(update: Update, context: CallbackContext):
                 title=song['full_title'],
                 url=song['lyrics_url'],
                 hide_url=True,
-                input_message_content=InputTextMessageContent(song['lyrics_url']+"\n"+"Check @GenyusBot for the lyrics!")
+                input_message_content=InputTextMessageContent(messageContent)
             )
         )
     context.bot.answer_inline_query(update.inline_query.id, results)
 
 
-def on_result_chosen(update: Update, context: CallbackContext):
-    songs=search(update.chosen_inline_result.query)
-    selected_index = update.chosen_inline_result.result_id
-    lyrics=search_lyrics(selected_index, songs)
-    bot.send_message(update.chosen_inline_result.from_user.id, text=lyrics)
+# In case you need to send a message to user when making the inline request
+
+# def on_result_chosen(update: Update, context: CallbackContext):
+#     print(update.to_dict())
+#     result = update.chosen_inline_result
+#     result_id = result.result_id
+#     query = result.query
+#     user = result.from_user.id
+#     print(result_id)
+#     print(user)
+#     print(query)
+#     print(result.inline_message_id)
+#     bot.send_message(update.chosen_inline_result.from_user.id, text="message to user")
 
 
 updater = Updater(token=TELEGRAM_BOT_TOKEN)
 dispatcher = updater.dispatcher
 
-inline_song_handler = InlineQueryHandler(inline_song, run_async=True)
+# song_handler = CommandHandler('song', song)
+# dispatcher.add_handler(song_handler)
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+inline_song_handler = InlineQueryHandler(inline_song, run_async=False)
 dispatcher.add_handler(inline_song_handler)
 
-result_chosen_handler = ChosenInlineResultHandler(on_result_chosen, run_async=True)
-dispatcher.add_handler(result_chosen_handler)
+# result_chosen_handler = ChosenInlineResultHandler(on_result_chosen, run_async=True)
+# dispatcher.add_handler(result_chosen_handler)
 
 updater.start_polling()
 
---
+++ scrape.py
@@ -0,0 +1,35 @@
+import requests, re, time
+from bs4 import BeautifulSoup
+import concurrent.futures
+
+
+global lyrics_searched
+lyrics_searched=dict()
+MAX_THREADS=30
+
+def find_lyrics(URL):
+    global lyrics_searched
+    lyrics_searched=dict()
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    lyrics = soup.find("div", class_="lyrics")
+    if(lyrics):
+        lyrics_searched[URL]=lyrics.get_text()
+        return lyrics.get_text()
+    else:
+        new_div = soup.find("div", class_=re.compile("Lyrics__Root"))
+        if not new_div:
+            lyrics_searched[URL]="No lyrics found"
+            return "No lyrics found"
+        lyrics = new_div.get_text('\n').replace('\n[', '\n\n[')
+        lyrics_searched[URL]=lyrics
+        return lyrics
+
+
+
+def get_lyrics(links):
+    threads=min(MAX_THREADS, len(links))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        executor.map(find_lyrics, links)
