ea076464afbfce1e6171bdf8b2fcadbef8e0c11f
Anas <ameziani@everis.com>
2021-01-11 20:54:50 +0100

    Command song working properly

--- botTelegram.py
+++ botTelegram.py
@@ -18,12 +18,12 @@ global lyrics_searched
 lyrics_searched=dict()
 
 
-def search(song_name):
+def search(song_name, inline=True):
     songs=genius.search_songs(song_name)
     list_songs={}
     lyrics_to_be_searched=list()
     for index, item in enumerate(songs['hits']):
-        if index>=5:
+        if inline and index>=5:
             break
         full_title=item['result']['full_title']
         lyrics_url=item['result']['url']
@@ -38,20 +38,29 @@ def search(song_name):
         attributes['artist']=artist
         
         list_songs[index]=attributes
-    global lyrics_searched
-    scrape.get_lyrics(lyrics_to_be_searched)
-    lyrics_searched=dict()
-    lyrics_searched=scrape.lyrics_searched
+    if inline:
+        global lyrics_searched
+        scrape.get_lyrics(lyrics_to_be_searched)
+        lyrics_searched=dict()
+        lyrics_searched=scrape.lyrics_searched
 
     return list_songs
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, I'm a genius bot. You can use me inline just by typing my name an the name of the song or artist i.e @GenyusBot stromae")
 
-# def song(update: Update, context: CallbackContext):
-#     song_name=re.sub(r"\/song ", "", update.message.text)
-#     result=search(song_name)
-#     context.bot.send_message(chat_id=update.effective_chat.id, text="[Google](www.google.es)", parse_mode='MarkdownV2')
+def song(update: Update, context: CallbackContext):
+    song_name=re.sub(r"\/song ", "", update.message.text)
+    songs=search(song_name, False)
+
+    result=""
+    pprint(songs)
+    for index, song in songs.items():
+        result+="<a href=\""+song['lyrics_url']+"\">"+"<b>"+str(index+1)+"</b>. "+song['full_title']+"</a>\n"
+
+    print(result)
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result, parse_mode=telegram.ParseMode.HTML, disable_web_page_preview=True)
 
 def inline_song(update: Update, context: CallbackContext):
     query=update.inline_query.query
@@ -103,8 +112,8 @@ def main():
     updater = Updater(token=TELEGRAM_BOT_TOKEN)
     dispatcher = updater.dispatcher
 
-    # song_handler = CommandHandler('song', song)
-    # dispatcher.add_handler(song_handler)
+    song_handler = CommandHandler('song', song)
+    dispatcher.add_handler(song_handler)
 
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
